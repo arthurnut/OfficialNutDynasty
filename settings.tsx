6c1401c547364d4b5d5f6e3750522cf9cd86d521
arthurnut <artbarn50@icloud.com>
2026-04-01 15:10:01 +0700

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -1,14 +1,11 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
-  { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
-  { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
+  { label: "default", mintText: "Mint Nut Dynasty for 0.05 SOL", buttonLabel: "Mint now!", header: "Nut Dynasty Mint" }
 ];
 
 //header image in the ui. replace with your own
-export const image = "https://avatars.githubusercontent.com/u/93528482?v=4";
+export const image = "https://raw.githubusercontent.com/arthurnut/OfficialNutDynasty/main/public/Logo_with_Nut_202603172032.jpg";
 
 //website title
-export const headerText = "MarkSackerberg's mint UI";
+export const headerText = "Nut Dynasty Mint";
